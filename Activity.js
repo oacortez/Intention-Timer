9a1086743a25591752bfa3050ed0e79712094794
Oscar Cortez <oacortez98@gmail.com>
2021-07-28 17:05:01 -0600

    Add Activity class blueprint

---
+++ Activity.js
@@ -0,0 +1,10 @@
+class Activity {
+  constructor(category,description,minutes,seconds) {
+    this.category = category;
+    this.description = description;
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.completed = false;
+    this.id = Date.now();
+  }
+}
