7433be4d0f6f2173c68d2541acabc1be2df4a93f
Renee Swick-Ziller <reneeswick@gmail.com>
2021-07-28 20:08:17 -0600

    Add toggling to each button

--- main.js
+++ main.js
@@ -12,17 +12,24 @@ exerciseBtn.addEventListener('click', changeExerciseColor);
 
 
 // ///////EVENT HANDLERS //////////////////////
+
 function changeStudyColor(){
   event.preventDefault();
   studyBtn.classList.toggle('study-button-active');
+  meditateBtn.classList.remove('meditate-button-active');
+  exerciseBtn.classList.remove('exercise-button-active');
 };
 
 function changeMeditateColor(){
   event.preventDefault();
   meditateBtn.classList.toggle('meditate-button-active');
+  studyBtn.classList.remove('study-button-active');
+  exerciseBtn.classList.remove('exercise-button-active');
 };
 
 function changeExerciseColor(){
   event.preventDefault();
   exerciseBtn.classList.toggle('exercise-button-active');
+  meditateBtn.classList.remove('meditate-button-active');
+  studyBtn.classList.remove('study-button-active');
 }
