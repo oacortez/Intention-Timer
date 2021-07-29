a80b95c79297674cdf3569aa28287c9c4ea74bcd
Renee Swick-Ziller <reneeswick@gmail.com>
2021-07-28 20:47:06 -0600

    Set limits and styling to minutes and seconds inputs

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
+// //////GLOBAL VARIABLES/////////////////////
+// var invalidChars = ['e', '+', '-', '.'];
+
 // //////QUERY SELECTORS///////////////////////
 var studyBtn = document.querySelector('#studyBtn');
 var meditateBtn = document.querySelector('#meditateBtn');
 var exerciseBtn = document.querySelector('#exerciseBtn');
-
+var minutesInput = document.querySelector('#minutes');
 
 
 // //////////EVENT LISTENERS ///////////////////
 studyBtn.addEventListener('click', changeStudyColor);
 meditateBtn.addEventListener('click', changeMeditateColor);
 exerciseBtn.addEventListener('click', changeExerciseColor);
+minutesInput.addEventListener('keydown', checkInvalidChars);
 
 
 // ///////EVENT HANDLERS //////////////////////
@@ -32,4 +36,12 @@ function changeExerciseColor(){
   exerciseBtn.classList.toggle('exercise-button-active');
   meditateBtn.classList.remove('meditate-button-active');
   studyBtn.classList.remove('study-button-active');
+};
+
+function checkInvalidChars(e){
+  var invalidChars = ['e', '+', '-', '.'];
+  if(invalidChars.includes(e.key)){
+    e.preventDefault();
+    console.log('it ran!!');
+  }
 }
